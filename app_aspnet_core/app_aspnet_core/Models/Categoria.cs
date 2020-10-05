ee0b1688651ebf4d51fba762de2c89326f3cd8a1
Matheus Santos <mcarneiro@sga.pucminas.br>
2020-10-05 17:57:51 -0300

    feat: added model Categoria

---
+++ app_aspnet_core/app_aspnet_core/Models/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_aspnet_core.Models
+{
+	// estamos sinalizando que vamos ter uma tabela no banco de dados 
+	[Table("Categorias")] 
+	public class Categoria
+	{
+		[Key] // PK e auto incremento 
+		[Display(Name ="Código")] // maneira que vai aparecer na minha view
+		public int Id { get; set; }
+
+		[Display(Name="Descrição")]
+		[Required(ErrorMessage ="Campo Obrigatório")]
+		public string Descricao { get; set; }
+	}
+}
